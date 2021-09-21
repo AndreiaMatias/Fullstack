382bb6fa2115f391be73a35467d75981d9aeb7cc
Andreia Matias <deiamat@yahoo.com>
2021-09-21 16:50:09 -0300

    calculadora

---
+++ calculadora.js
@@ -0,0 +1,9 @@
+let divisao = require("./dividir");
+let multiplicacao = require("./multiplicar");
+let soma = require("./somar");
+let subtracao = require("./subtrair");
+
+console.log(soma(4,2));
+console.log(subtracao(5,2));
+console.log(multiplicacao(2,2));
+console.log(divisao(10,2));
