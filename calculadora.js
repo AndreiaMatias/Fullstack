7d9ae6cab45f727e7dd12e5daec1ea1400d4e5db
Andreia Matias <deiamat@yahoo.com>
2021-09-21 17:00:55 -0300

    atualização calculadora

--- calculadora.js
+++ calculadora.js
@@ -3,7 +3,7 @@ let multiplicacao = require("./multiplicar");
 let soma = require("./somar");
 let subtracao = require("./subtrair");
 
-console.log(soma(4,2));
-console.log(subtracao(5,2));
-console.log(multiplicacao(2,2));
-console.log(divisao(10,2));
+console.log("Soma: " + soma(4,2));
+console.log("Subtração: " + subtracao(5,2));
+console.log("Multiplicação: " + multiplicacao(2,2));
+console.log("Divisão: " + divisao(10,2));
